1a904ad391f4d27220572856129cb783f3ec417e
OIslam53 <omer.islam53@hotmail.com>
2024-11-18 10:46:47 +0300

    changed visuals a bit & removed unnessary class

--- ZestApp/app/(tabs)/index.tsx
+++ ZestApp/app/(tabs)/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import {StyleSheet, View, Text, ScrollView, Pressable} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, Pressable, Dimensions} from 'react-native';
 import {Link, useNavigation} from "expo-router";
-import Ionicons from "@expo/vector-icons/Ionicons";
 import {FontAwesome6, MaterialCommunityIcons, MaterialIcons} from "@expo/vector-icons";
-import UserAccountCard from "@/components/AccountCard";
 import {SafeAreaView} from "react-native-safe-area-context";
+import Carousel from "react-native-snap-carousel";
 
 interface TransactionHistory{
     id: number;
     amount: number;
     date: string;
     type: string;
+}
+interface Cards {
+    id: number;
+    name: string;
+    balance: number;
+
+}
+
+interface UserAccount {
+    id: number;
+    name: string;
+    cards: Cards[];
 }
 
 const mockHistory: TransactionHistory[] = []
@@ -31,12 +42,48 @@ export default function HomeScreen() {
     mockAll.push(hist4);
 
     //All should be visible in the transaction history
+    let card_list: Cards[] = []
+    const card_1 = {id: 1, name: 'Card 1', balance: 1000};
+    const card_2 = {id: 2, name: 'Card 2', balance: 2000};
+    const card_3 = {id: 3, name: 'Card 3', balance: 3000};
+    card_list.push(card_1)
+    card_list.push(card_2)
+    card_list.push(card_3)
+
+    const userAccountData: UserAccount = {
+        id: 1,
+        name: 'John Doe',
+        cards: card_list,
+    };
 
     const navigation = useNavigation();
-
     return (
+        //TODO: Keep the selected wallet id in the state and pass it to the wallet details page
         <SafeAreaView style={styles.container}>
-            <UserAccountCard userId={1}/>
+            <Carousel
+                data={userAccountData.cards}
+                renderItem={({item, index}) => {
+                    return (
+                        <View style={
+                            {
+                                backgroundColor: 'white',
+                                borderRadius: 10,
+                                padding: 20,
+                                marginLeft: 10,
+                                marginRight: 10,
+                                height: 200,
+                                borderColor: "#555555",
+                                borderWidth: 3,
+                            }}>
+                            <Text>{item.name}</Text>
+                            <Text>{item.balance}</Text>
+                        </View>
+                    );
+                }}
+                sliderWidth={Dimensions.get("screen").width}
+                itemWidth={Dimensions.get("screen").width * 0.8}
+                vertical={false}
+            />
             <View style={styles.walletAction}>
                 <Link href="../other/atomicSwap" asChild>
                     <Pressable style={styles.circularButton}>
@@ -78,7 +125,7 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "lightblue",
+        backgroundColor: "lightyellow",
         alignItems: "center",
         justifyContent: "flex-start",
         padding: 10,
@@ -115,6 +162,8 @@ const styles = StyleSheet.create({
         width: 75,
         height: 75,
         borderRadius: 70,
+        borderColor: "black",
+        borderWidth: 1,
         backgroundColor: 'lightgrey',
         justifyContent: 'space-around',
         padding: 23,
