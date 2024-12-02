1cb5b664f90ce2b14808c9f3c7efc52936f05160
odogancay-ku <91738862+odogancay-ku@users.noreply.github.com>
2024-12-02 11:35:54 +0300

    Feature/backend initialization (#7)
    Added boilerplate code and dependency setup for backend

---
+++ ZestBackend/src/main/java/org/zest/zestbackend/model/TransactionDTO.java
@@ -0,0 +1,12 @@
+package org.zest.zestbackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TransactionDTO {
+
+}
---
+++ ZestBackend/src/main/java/org/zest/zestbackend/service/TransactionService.java
@@ -0,0 +1,7 @@
+package org.zest.zestbackend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionService {
+}
