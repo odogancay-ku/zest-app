d44d55b58f913f314cb88c32723cd79212656e02
odogancay-ku <odogancay21@ku.edu.tr>
2024-12-02 15:28:00 +0300

    Update atomic swap to use react-native-paper styles

--- ZestApp/app/other/atomicSwap.tsx
+++ ZestApp/app/other/atomicSwap.tsx
@@ -1,35 +1,15 @@
-import {View,StyleSheet,Text} from "react-native";
-import {useRouter,Stack} from "expo-router";
-import {useEffect} from "react";
+import React from 'react';
+import {View} from 'react-native';
+import {Appbar, Text, Surface, useTheme} from 'react-native-paper';
+import {SafeAreaView} from "react-native-safe-area-context";
 
 export default function AtomicSwap() {
-
     return (
-        <View style={styles.container}>
-            <Stack.Screen
-                options={{
-                    title: 'Swap',
-                    headerStyle: { backgroundColor: '#f4511e' },
-                    headerTintColor: '#fff',
-                    headerTitleStyle: {
-                        fontWeight: 'bold',
-                    },
-                }}
-            />
-                <Text>AtomicSwap</Text>
-        </View>
+        <SafeAreaView style={{flex: 1, padding: 10, backgroundColor: useTheme().colors.background}}>
+            <Text variant="headlineLarge">Atomic Swap</Text>
+            <Text variant="bodyMedium">
+                This page uses only react-native-paper components and adheres to its theme defaults.
+            </Text>
+        </SafeAreaView>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        padding: 20,
-        backgroundColor: "#f0f0f0",
-    },
-    header: {
-        fontSize: 24,
-        fontWeight: "bold",
-        marginBottom: 20,
-    },
-});
