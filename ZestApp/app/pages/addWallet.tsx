6db267d150e38847fec5ebf23df2347454c7004a
omerislam <omer.islam@finartz.com>
2024-12-04 11:21:16 +0300

    some ui fixes

--- ZestApp/app/pages/addWallet.tsx
+++ ZestApp/app/pages/addWallet.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Stack, useNavigation, useRouter } from 'expo-router';
-import { TextInput, Button, Text, useTheme, Surface } from 'react-native-paper';
+import React, {useState} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {Stack, useNavigation, useRouter} from 'expo-router';
+import {TextInput, Button, Text, useTheme, Surface} from 'react-native-paper';
+import {Picker} from "@react-native-picker/picker";
 
 export default function AddWallet() {
     const [walletName, setWalletName] = useState('');
@@ -9,7 +10,7 @@ export default function AddWallet() {
     const router = useRouter();
     const navigation = useNavigation();
     const theme = useTheme();
-
+    const [selectedCurrency, setSelectedCurrency] = useState<string>("BTC");
     const saveWallet = () => {
         const newWallet = {
             id: Math.random(), // Generate a unique ID for the new wallet
@@ -22,12 +23,12 @@ export default function AddWallet() {
     };
 
     return (
-        <SafeAreaView style={{ flex: 1, padding: 16, gap: 20, backgroundColor: theme.colors.background }}>
+        <SafeAreaView style={{flex: 1, padding: 16, gap: 20, backgroundColor: theme.colors.background}}>
             {/* Header */}
             <Stack.Screen
                 options={{
                     title: 'Add New Wallet',
-                    headerStyle: { backgroundColor: theme.colors.primary },
+                    headerStyle: {backgroundColor: theme.colors.primary},
                     headerTintColor: theme.colors.onPrimary,
                     headerTitleStyle: {
                         fontWeight: 'bold',
@@ -35,42 +36,47 @@ export default function AddWallet() {
                 }}
             />
 
-                {/* Wallet Name Input */}
-                <Text variant="titleMedium">Wallet Name</Text>
-                <TextInput
-                    mode="outlined"
-                    label="Enter wallet name"
-                    value={walletName}
-                    onChangeText={setWalletName}
-                />
+            {/* Wallet Name Input */}
+            <Text variant="titleMedium">Wallet Name</Text>
+            <TextInput
+                mode="outlined"
+                label="Enter wallet name"
+                value={walletName}
+                onChangeText={setWalletName}
+            />
 
-                {/* Wallet Network Input */}
-                <Text variant="titleMedium">Wallet Network</Text>
-                <TextInput
-                    mode="outlined"
-                    label="Enter wallet network"
-                    value={walletNetwork}
-                    onChangeText={setWalletNetwork}
-                />
-
-                {/* Private Key Keywords Button */}
-                <Button
-                    mode="contained"
-                    onPress={() => {
-                        router.push(
-                            {
-                                pathname: './privateKeywords',
-                                params: { network: walletNetwork },
-                            });
-                    }}
+            {/* Wallet Network Input */}
+            {/* Currency Picker */}
+            <Surface style={{padding: 16, elevation: 2}}>
+                <Text variant="headlineSmall">Choose Currency</Text>
+                <Picker
+                    selectedValue={selectedCurrency}
+                    onValueChange={(itemValue) => setSelectedCurrency(itemValue)}
                 >
-                    Insert Private Key Keywords
-                </Button>
+                    <Picker.Item label="BTC - Bitcoin" value="BTC"/>
+                    <Picker.Item label="ETH - Ethereum" value="ETH"/>
+                </Picker>
+                <Text>Selected Currency: {selectedCurrency}</Text>
+            </Surface>
 
-                {/* Save Wallet Button */}
-                <Button mode="contained" onPress={saveWallet}>
-                    Save Wallet
-                </Button>
+            {/* Private Key Keywords Button */}
+            <Button
+                mode="contained"
+                onPress={() => {
+                    router.push(
+                        {
+                            pathname: './privateKeywords',
+                            params: {network: walletNetwork},
+                        });
+                }}
+            >
+                Insert Private Key Keywords
+            </Button>
+
+            {/* Save Wallet Button */}
+            <Button mode="contained" onPress={saveWallet}>
+                Save Wallet
+            </Button>
         </SafeAreaView>
     );
 }
--- ZestApp/app/pages/privateKeywords.tsx
+++ ZestApp/app/pages/privateKeywords.tsx
@@ -1,24 +1,33 @@
 import {Stack, useLocalSearchParams, useRouter} from "expo-router";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {SafeAreaView} from "react-native-safe-area-context";
 import {TextInput, Button, Text, useTheme, Switch} from 'react-native-paper';
 import {ScrollView, View} from "react-native";
 
 export default function PrivateKeywords() {
-    const [privateKeywords, setPrivateKeywords] = useState<string[]>(new Array(12).fill(''));
-    const [privateKey, setPrivateKey] = useState('');
+    const [mnemonicPhrase, setMnemonicPhrase] = useState<string[]>(new Array(12).fill(''));
+    const [key, setKey] = useState('');
     const [inputType, setInputType] = useState(true);
     const {network} = useLocalSearchParams<{network:string}>();
     const router = useRouter();
     const theme = useTheme();
 
+
+
     const validateKeysAndNavigate = () => {
+        let m = inputType?mnemonicPhrase.join(' '):"";
+        let k = inputType?"":key
         router.navigate({
             pathname: './addWallet',
-            params: {privateKey: privateKey}
+            params: {mnemonic: m, key: k}
         })
     }
 
+    useEffect(() => {
+        console.log(network)
+        let test_mnemonic = "praise you muffin lion enable neck grocery crumble super myself license ghost"
+        setMnemonicPhrase(test_mnemonic.split(' '))
+    }, []);
 
     const toggleSwitch = () => setInputType(previousState => !previousState);
     return (
@@ -39,25 +48,25 @@ export default function PrivateKeywords() {
             <Text variant="titleMedium">Enter your mnemonic key keywords</Text>
             <View style={{flex:1}}>
             {inputType ? <ScrollView>
-                    {privateKeywords.map((keyword, index) => (
+                    {mnemonicPhrase.map((keyword, index) => (
                         index % 2 === 0 && (
                             <View key={index}>
                                 <TextInput
-                                    value={privateKeywords[index]}
+                                    value={mnemonicPhrase[index]}
                                     onChangeText={(text) => {
-                                        const newKeywords = [...privateKeywords];
+                                        const newKeywords = [...mnemonicPhrase];
                                         newKeywords[index] = text;
-                                        setPrivateKeywords(newKeywords);
+                                        setMnemonicPhrase(newKeywords);
                                     }}
                                     placeholder={`Enter keyword ${index + 1}`}
                                 />
-                                {index + 1 < privateKeywords.length && (
+                                {index + 1 < mnemonicPhrase.length && (
                                     <TextInput
-                                        value={privateKeywords[index + 1]}
+                                        value={mnemonicPhrase[index + 1]}
                                         onChangeText={(text) => {
-                                            const newKeywords = [...privateKeywords];
+                                            const newKeywords = [...mnemonicPhrase];
                                             newKeywords[index + 1] = text;
-                                            setPrivateKeywords(newKeywords);
+                                            setMnemonicPhrase(newKeywords);
                                         }}
                                         placeholder={`Enter keyword ${index + 2}`}
                                     />
@@ -67,8 +76,8 @@ export default function PrivateKeywords() {
                     ))}
                 </ScrollView> :
                 <TextInput
-                    value={privateKey}
-                    onChangeText={setPrivateKey}
+                    value={key}
+                    onChangeText={setKey}
                     placeholder="Enter your mnemonic key"/>}
             </View>
             <Button
