43a342df02a483fce08307279f69470c1a375dbe
omerislam <omer.islam@finartz.com>
2024-12-05 13:24:58 +0300

    fixed removing wallet, and stack ordering

--- ZestApp/app/(tabs)/index.tsx
+++ ZestApp/app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Dimensions, ScrollView, TouchableOpacity, View } from 'react-native';
-import { Link, useRouter } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Card, Text, IconButton, Divider, useTheme } from 'react-native-paper';
+import React, {useEffect, useState} from 'react';
+import {Dimensions, ScrollView, TouchableOpacity, View} from 'react-native';
+import {Link, useFocusEffect, useRouter} from "expo-router";
+import {SafeAreaView} from "react-native-safe-area-context";
+import {Card, Text, IconButton, Divider, useTheme} from 'react-native-paper';
 import Carousel from "react-native-snap-carousel";
-import { FontAwesome6, MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import {FontAwesome6, MaterialCommunityIcons, MaterialIcons} from "@expo/vector-icons";
 import CircleButton from "@/app/widgets/CircleButton";
 import * as SecureStore from 'expo-secure-store';
 
@@ -22,10 +22,10 @@ interface TransactionHistory {
 }
 
 const mockAll: TransactionHistory[] = [
-    { walletId: "1", amount: 100, date: '2021-09-01', type: 'deposit' },
-    { walletId: "1", amount: 200, date: '2021-09-02', type: 'withdraw' },
-    { walletId: "2", amount: 300, date: '2021-09-03', type: 'deposit' },
-    { walletId: "3", amount: 400, date: '2021-09-04', type: 'withdraw' },
+    {walletId: "1", amount: 100, date: '2021-09-01', type: 'deposit'},
+    {walletId: "1", amount: 200, date: '2021-09-02', type: 'withdraw'},
+    {walletId: "2", amount: 300, date: '2021-09-03', type: 'deposit'},
+    {walletId: "3", amount: 400, date: '2021-09-04', type: 'withdraw'},
 ];
 
 export default function HomeScreen() {
@@ -34,47 +34,51 @@ export default function HomeScreen() {
     const router = useRouter();
     const theme = useTheme();
 
-    useEffect(() => {
-        // SecureStore.deleteItemAsync('wallets').then(() => console.log('Wallets deleted'));
+    useFocusEffect( React.useCallback(() => {
         const fetchWallets = async () => {
             const storedWallets = await SecureStore.getItemAsync('wallets');
+            console.log(storedWallets);
             if (!storedWallets) {
-                router.replace('/pages/addWallet');
+                setWallets([]); //to make it more consistent
+                router.push('/pages/addWallet');
                 return;
             }
             setWallets(JSON.parse(storedWallets));
         };
         fetchWallets()
-    }, []);
+    }, []));
 
     const filteredTransactions = mockAll.filter(transaction => transaction.walletId === wallets[selectedWalletIndex]?.id);
 
     return (
-        <SafeAreaView style={{ flexDirection: "column", gap: 10, padding: 10, backgroundColor: theme.colors.background }}>
+        <SafeAreaView style={{flexDirection: "column", gap: 10, padding: 10, backgroundColor: theme.colors.background}}>
             <Carousel
-                data={[...wallets, { id: "", name: "Add Wallet", balance: 0 }]}
-                renderItem={({ item, index }) => (
+                data={[...wallets, {id: "", name: "Add Wallet", balance: 0}]}
+                renderItem={({item, index}) => (
                     item.id === "" ? (
                         <Card
-                            style={{ height: 200, backgroundColor: theme.colors.primaryContainer }}
+                            style={{height: 200, backgroundColor: theme.colors.primaryContainer}}
                             onPress={() => router.push('/pages/addWallet')}
                         >
-                            <Card.Content style={{ alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                            <Card.Content style={{alignItems: 'center', justifyContent: 'center', height: '100%'}}>
                                 <IconButton
                                     icon="plus"
                                     size={40}
-                                    style={{ borderWidth: 1, backgroundColor: theme.colors.onPrimary }}
+                                    style={{borderWidth: 1, backgroundColor: theme.colors.onPrimary}}
                                 />
                             </Card.Content>
                         </Card>
                     ) : (
                         <TouchableOpacity
-                            onPress={() => router.push({ pathname: '/pages/walletDetails', params: { selectedWalletIndex: index } })}
+                            onPress={() => router.push({
+                                pathname: '/pages/walletDetails',
+                                params: {selectedWalletIndex: index}
+                            })}
                         >
                             <Card style={{
                                 height: 200, backgroundColor: theme.colors.primaryContainer
                             }}>
-                                <Card.Content style={{ height: '100%' }}>
+                                <Card.Content style={{height: '100%'}}>
                                     <Text variant="headlineSmall">{item.name}</Text>
                                     <Text>{item.balance}</Text>
                                 </Card.Content>
@@ -90,7 +94,7 @@ export default function HomeScreen() {
                 }}
             />
 
-            <ScrollView horizontal={true} style={{ height: 100, width: '100%' }}
+            <ScrollView horizontal={true} style={{height: 100, width: '100%'}}
                         contentContainerStyle={{
                             flexDirection: 'row', justifyContent: 'center', alignItems: 'center',
                             gap: 30,
@@ -98,26 +102,26 @@ export default function HomeScreen() {
                         }}
                         showsHorizontalScrollIndicator={false}>
 
-                <Link href={{ pathname: "/pages/atomicSwap" }} asChild>
+                <Link href={{pathname: "/pages/atomicSwap"}} asChild>
                     <CircleButton>
-                        <MaterialIcons name="currency-exchange" size={30} color="black" />
+                        <MaterialIcons name="currency-exchange" size={30} color="black"/>
                     </CircleButton>
                 </Link>
-                <Link href={{ pathname: "/pages/transaction" }} asChild>
+                <Link href={{pathname: "/pages/transaction"}} asChild>
                     <CircleButton>
-                        <FontAwesome6 name="money-bill-transfer" size={30} color="black" />
+                        <FontAwesome6 name="money-bill-transfer" size={30} color="black"/>
                     </CircleButton>
                 </Link>
-                <Link href={{ pathname: "/pages/walletDetails", params: { selectedWalletIndex } }} asChild>
+                <Link href={{pathname: "/pages/walletDetails", params: {selectedWalletIndex}}} asChild>
                     <CircleButton>
-                        <MaterialCommunityIcons name="card-account-details" size={30} color="black" />
+                        <MaterialCommunityIcons name="card-account-details" size={30} color="black"/>
                     </CircleButton>
                 </Link>
             </ScrollView>
 
-            <Card style={{ padding: 10 }}>
-                <Card.Title title="Transaction History" />
-                <Divider />
+            <Card style={{padding: 10}}>
+                <Card.Title title="Transaction History"/>
+                <Divider/>
                 <ScrollView>
                     {filteredTransactions.map((transaction) => (
                         <View key={transaction.date} style={{
@@ -127,10 +131,10 @@ export default function HomeScreen() {
                             borderBottomWidth: 1,
                             borderColor: '#eee'
                         }}>
-                            <Text style={{ flex: 1 }}>{transaction.walletId}</Text>
-                            <Text style={{ flex: 1 }}>{transaction.amount}</Text>
-                            <Text style={{ flex: 1 }}>{transaction.date}</Text>
-                            <Text style={{ flex: 1 }}>{transaction.type}</Text>
+                            <Text style={{flex: 1}}>{transaction.walletId}</Text>
+                            <Text style={{flex: 1}}>{transaction.amount}</Text>
+                            <Text style={{flex: 1}}>{transaction.date}</Text>
+                            <Text style={{flex: 1}}>{transaction.type}</Text>
                         </View>
                     ))}
                 </ScrollView>
